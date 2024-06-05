5d5dbc2ce1eaaf47fac9b6914d36760c7d5fc484
0kenx <km@nxfi.app>
2024-06-05 07:58:52 +0200

    update test
    Signed-off-by: 0kenx <km@nxfi.app>

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,13 +14,13 @@ describe('ton-compiler', () => {
   it('should crash if sources are not under working dir', async () => {
     await expect(compileContract({ files: [path.resolve(__dirname, 'tests', 'test.fc')], version: 'legacy', workdir: path.resolve(__dirname, 'engines') })).rejects.toThrowError();
   });
-  it('should compile source with legacy compiler', async () => {
+  it.skip('should compile source with legacy compiler', async () => {
     let compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'test.fc')], version: 'legacy' });
     expect(compiled).toMatchSnapshot();
     compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'global.fc')], version: 'legacy' });
     expect(compiled).toMatchSnapshot();
   });
-  it('should compile with errors with legacy compiler', async () => {
+  it.skip('should compile with errors with legacy compiler', async () => {
     let compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'test_bug.fc')], version: 'legacy' });
     expect(compiled).toMatchSnapshot();
   });
@@ -52,6 +52,16 @@ describe('ton-compiler', () => {
   });
   it('should compile with errors with v2023.01 compiler', async () => {
     let compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'test_bug.fc')], version: 'v2023.01' });
+    expect(compiled).toMatchSnapshot();
+  });
+  it('should compile source with v2024.01 compiler', async () => {
+    let compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'test.fc')], version: 'v2024.01' });
+    expect(compiled).toMatchSnapshot();
+    compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'global.fc')], version: 'v2024.01' });
+    expect(compiled).toMatchSnapshot();
+  });
+  it('should compile with errors with v2024.01 compiler', async () => {
+    let compiled = await compileContract({ files: [path.resolve(__dirname, 'tests', 'test_bug.fc')], version: 'v2024.01' });
     expect(compiled).toMatchSnapshot();
   });
 });
